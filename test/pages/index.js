2701d4b04a10a3ff7c91a38190ed4782ab6ecade
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-22 17:06:54 +0000

    aa

--- test/pages/index.js
+++ test/pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Navbar from './layout/Navbar'
 
 export default function Home() {
@@ -11,15 +9,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
       </head>
 
-      <body>
         <Navbar/>
-
-
-        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
-      </body>
-
     </div>
   )
 }
