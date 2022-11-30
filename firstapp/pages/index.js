fda990d6212c4c1915725418519142fe2ac1bcae
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2022-11-30 11:24:19 +0000

    Update index.js

--- firstapp/pages/index.js
+++ firstapp/pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+        <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
       </head>
 
       <body>
