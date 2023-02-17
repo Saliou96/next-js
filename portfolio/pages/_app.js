96bf0216ded393416738c7841cd82f180bd60caa
Serigne Saliou Mbacké NDIAYE <68548037+Saliou96@users.noreply.github.com>
2023-02-17 09:28:59 +0000

    h

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
